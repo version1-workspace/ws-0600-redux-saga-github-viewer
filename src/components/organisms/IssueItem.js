9df2b29d58625a93792b9ae22cd3ae01ec7b04b6
jiro <j.code.public@gmail.com>
2020-05-07 15:49:54 -0700

    some fix

--- src/components/organisms/IssueItem.js
+++ src/components/organisms/IssueItem.js
@@ -1,8 +1,8 @@
-import React, { useCallback } from 'react'
-import styled from 'styled-components'
-import PropTypes from 'prop-types'
-import dayjs from 'dayjs'
-import { colors } from '../../styles/variable'
+import React, {useCallback} from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import dayjs from 'dayjs';
+import {colors} from '../../styles/variable';
 
 const Container = styled.tr`
   cursor: pointer;
@@ -10,45 +10,55 @@ const Container = styled.tr`
   &:hover {
     background: ${colors.hoverRow};
   }
-`
+`;
 
-const onClickLink = e => e.stopPropagation()
+const onClickLink = e => e.stopPropagation();
+const noop = () => undefined;
 
-const IssueItem = ({ checked, onClick, onCheck, issue }) => {
-  const { id, title, state, user, htmlUrl, createdAt, updatedAt } = issue
+const IssueItem = ({checked, onClick, onCheck, issue}) => {
+  const {id, title, state, user, htmlUrl, createdAt, updatedAt} = issue;
   const _onClick = useCallback(
-    (e) => {
-      onClick(issue)
+    e => {
+      onClick(issue);
     },
-    [issue, onClick]
-  )
+    [issue, onClick],
+  );
   const _onCheck = useCallback(
-    (e) => {
-      e.stopPropagation()
-      onCheck(issue)
+    e => {
+      e.stopPropagation();
+      onCheck(issue);
     },
-    [issue, onCheck]
-  )
+    [issue, onCheck],
+  );
 
   return (
     <Container key={id} onClick={_onClick}>
       <td>
-        <input type="checkbox" defaultChecked={checked} onClick={_onCheck} />
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={noop}
+          onClick={_onCheck}
+        />
       </td>
-      <td className="outline"><a onClick={onClickLink} href={htmlUrl}>{title}</a></td>
+      <td className="outline">
+        <a onClick={onClickLink} href={htmlUrl}>
+          {title}
+        </a>
+      </td>
       <td>{state}</td>
       <td>{user.login}</td>
       <td>{dayjs(createdAt).format('MM-DD-YYYY')}</td>
       <td>{dayjs(updatedAt).format('MM-DD-YYYY')}</td>
     </Container>
-  )
-}
+  );
+};
 
 IssueItem.propTypes = {
   checked: PropTypes.bool,
   onCheck: PropTypes.func,
   onClick: PropTypes.func,
-  issue: PropTypes.object
-}
+  issue: PropTypes.object,
+};
 
-export default IssueItem
+export default IssueItem;
